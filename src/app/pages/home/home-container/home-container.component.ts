ea350de5a5e3dbfc57da66511cdf0b1b7a49e2f7
Santiago <edy.valencia@prevsis.com>
2025-01-09 08:03:17 -0500

    add PrimeNG integration with button, dialog, and listbox components; update styles and configuration

--- src/app/pages/home/home-container/home-container.component.ts
+++ src/app/pages/home/home-container/home-container.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core'
 import { Title } from '@angular/platform-browser'
 
+interface City {
+  name: string,
+  code: string
+}
+
+
 @Component({
   selector: 'app-home-container',
   templateUrl: './home-container.component.html',
@@ -8,11 +14,24 @@ import { Title } from '@angular/platform-browser'
   standalone: false
 })
 export class HomeContainerComponent implements OnInit {
+  display: boolean = true;
+  cities!: City[];
+  selectedCity!: City;
+
   private _titleService = inject(Title)
 
   constructor() {
     this._titleService.setTitle('Inicio')
+
   }
 
-  ngOnInit() {}
+   ngOnInit() {
+        this.cities = [
+            { name: 'New York', code: 'NY' },
+            { name: 'Rome', code: 'RM' },
+            { name: 'London', code: 'LDN' },
+            { name: 'Istanbul', code: 'IST' },
+            { name: 'Paris', code: 'PRS' }
+        ];
+    }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SharedModule } from 'src/app/shared/shared.module'
 import { HomeContainerComponent } from './home-container/home-container.component'
+import { ButtonModule } from 'primeng/button'
+import { DialogModule } from 'primeng/dialog'
+import { ListboxModule } from 'primeng/listbox';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
-  imports: [CommonModule, SharedModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    ButtonModule,
+    DialogModule,
+    FormsModule,
+    ListboxModule
+  ],
   declarations: [HomeContainerComponent]
 })
 export class HomeModule {}
