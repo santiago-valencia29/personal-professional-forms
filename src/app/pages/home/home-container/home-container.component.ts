dcaa028dba3e77340a85ef5bd968aa42884f6f89
Santiago <edy.valencia@prevsis.com>
2025-01-09 17:05:53 -0500

    refactor: update theme style and simplify home container component

--- src/app/pages/home/home-container/home-container.component.ts
+++ src/app/pages/home/home-container/home-container.component.ts
@@ -15,70 +15,12 @@ interface City {
 })
 export class HomeContainerComponent implements OnInit {
   display: boolean = true
-  cities!: City[]
-  selectedCity!: City
-  items: MenuItem[]
 
   private _titleService = inject(Title)
 
   constructor(private messageService: MessageService) {
     this._titleService.setTitle('Inicio')
-    this.items = [
-      {
-        label: 'Update',
-        command: () => {
-          this.update()
-        }
-      },
-      {
-        label: 'Delete',
-        command: () => {
-          this.delete()
-        }
-      },
-      { label: 'Angular Website', url: 'http://angular.io' },
-      { separator: true },
-      { label: 'Upload', routerLink: ['/fileupload'] }
-    ]
   }
 
-  ngOnInit() {
-    this.cities = [
-      { name: 'New York', code: 'NY' },
-      { name: 'Rome', code: 'RM' },
-      { name: 'London', code: 'LDN' },
-      { name: 'Istanbul', code: 'IST' },
-      { name: 'Paris', code: 'PRS' }
-    ]
-    // this.cities = []
-    this.getCities()
-  }
-
-  getCities() {
-    this.cities
-  }
-
-  save(severity: string) {
-    this.messageService.add({
-      severity: severity,
-      summary: 'Success',
-      detail: 'Data Saved'
-    })
-  }
-
-  update() {
-    this.messageService.add({
-      severity: 'success',
-      summary: 'Success',
-      detail: 'Data Updated'
-    })
-  }
-
-  delete() {
-    this.messageService.add({
-      severity: 'success',
-      summary: 'Success',
-      detail: 'Data Deleted'
-    })
-  }
+  ngOnInit() {}
 }
